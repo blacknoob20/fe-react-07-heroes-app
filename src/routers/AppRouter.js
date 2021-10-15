1a0f65ea84216141cd67c3c0e8cad473fc700c94
Cristhian Guerrero <cristian.guerrero@guayas.gob.ec>
2021-10-14 23:47:30 -0500

    Proyecto Heroes (SPA) | Rutas privadas.

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { LoginPage } from '../components/login/LoginPage';
 import { DashboardRoutes } from './DashboardRoutes';
+import { PrivateRoute } from './PrivateRoute';
+import { AuthContext } from '../auth/AuthContext';
 
 export const AppRouter = () => {
+    const { user } = useContext(AuthContext);
+
     return (
         <Router>
             <div>
                 {/* <Navbar /> */}
                 <Switch>
                     <Route path="/login" component={LoginPage} />
-                    <Route path="/" component={DashboardRoutes} />
+                    <PrivateRoute path="/" component={DashboardRoutes} isAuthenticated={user.logged} />
                 </Switch>
             </div>
         </Router>
