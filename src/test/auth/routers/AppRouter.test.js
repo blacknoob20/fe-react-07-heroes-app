e39e48a596751593bc0980b74589e60a508c0eff
Cristhian Guerrero <cristian.guerrero@guayas.gob.ec>
2021-10-30 03:59:14 -0500

    Proyecto Heroes (SPA) | Pruebas en el <AppRouter />.

---
+++ src/test/auth/routers/AppRouter.test.js
@@ -0,0 +1,43 @@
+import { mount, shallow } from 'enzyme';
+import { AuthContext } from '../../../auth/AuthContext';
+import { AppRouter } from '../../../routers/AppRouter';
+
+describe('Pruebas en <AppRouter/>', () => {
+    const contextValue = {
+        dispach: jest.fn(),
+        user: {
+            logged: false
+        }
+    }
+
+    test('Debe de mostrar el login si no esta autenticado.', () => {
+        const wrapper = mount(
+            <AuthContext.Provider value={contextValue}>
+                <AppRouter />
+            </AuthContext.Provider>
+        );
+
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('Debe mostrar el componente marvel si esta autenticado.', () => {
+        const contextValue = {
+            dispach: jest.fn(),
+            user: {
+                logged: true,
+                name: 'Cristhian'
+            }
+        }
+
+        const wrapper = mount(
+            <AuthContext.Provider value={contextValue}>
+                <AppRouter />
+            </AuthContext.Provider>
+        );
+
+        expect(wrapper.find('.navbar').exists()).toBe(true);
+
+    });
+
+
+});
